853218565de9ac6af028154bc122cc1bb5311dc2
Daniel Corson <danl@alum.mit.edu>
2009-10-06 16:39:53 -0700

    irename working; no pre-move safety

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,25 @@
-module Main where
+import Control.Applicative
+import Control.Monad
+import FUtil
+import System.Directory
+import System.Environment
+import System.Process
 
-main :: IO ()
+-- don't actually need " stuff..
+encode = show
+
 main = do
-  putStrLn "hi"
+  args <- getArgs
+  -- fixme
+  let tempF = "tmp"
+  cs <- zip args . map encode <$> mapM readFile args
+  writeFile tempF . unlines $ map (\ (a, b) -> a ++ "\t" ++ b) cs
+  p <- runProcess "vim" [tempF] Nothing Nothing Nothing Nothing Nothing
+  waitForProcess p
+  ls <- filter (not . null) . map (takeWhile (/= '\t')) . lines <$>
+    readFile tempF
+  if length ls == length args
+    then mapM_ (\ (old, new) -> when (old /= new) $ renameFile old new) $
+      zip args ls
+    else error "Line count mismatch, aborting without moving any files."
+
